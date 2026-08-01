99f6f137d56e7863d50db0375d62f37f8dfb5142
OLDPEI <mrpeijixing@gmail.com>
2026-08-01 19:13:50 +0800

    删除cname

--- assets/ads.js
+++ assets/ads.js
@@ -6,13 +6,6 @@ const AD_LIST = [
     code: "https://pan.baidu.com/s/1ocMaiLN4Or1PNXEDmx4agQ?pwd=x82g 提取码: x82g 复制这段内容后打开百度网盘手机App，操作更方便哦\n口令：怆忾丛享茯丰皎尖视变氧凸弋",
     platform: "任推邦",
     desc: "可单机可联机，断网也能玩"
-  },
-  {
-    id: 1,
-    title: "Giacomo LOLS2赛季 服务端及客户端",
-    code: "https://pan.baidu.com/s/1ocMaiLN4Or1PNXEDmx4agQ?pwd=x82g 提取码: x82g 复制这段内容后打开百度网盘手机App，操作更方便哦\n口令：怆忾丛享茯丰皎尖视变氧凸弋",
-    platform: "任推邦",
-    desc: "可单机可联机，断网也能玩"
   }
   // 可继续添加资源...
 ];
